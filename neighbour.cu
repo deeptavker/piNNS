c473b60fd2ccaed00dd9de4b254aa2ef14fbe567
Deep Tavker <tavkerdeep@gmail.com>
2020-09-07 23:23:18 +0530

    Update neighbour.cu

--- neighbour.cu
+++ neighbour.cu
@@ -1,6 +1,8 @@
 /*
 
 Copyright (C) 2018-2020 Deep Tavker (tavkerdeep@gmail.com)
+Copyright (C) 2018-2020 Ahmad Shakibaenia
+Copyright (C) 2018-2020 Mojtaba Jandaghian
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
